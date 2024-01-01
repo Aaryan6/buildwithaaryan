655d403eceb8d99f4274e4aca4fe1b54ff617b44
Aaryan6 <aaryanpatel683@gmail.com>
2024-01-01 10:30:18 +0530

    clean the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-"use client";
-
-import useStore from "@/lib/zustand";
-
 export default function Home() {
-  const name = useStore((state) => state.name);
-  const bio = useStore((state) => state.bio);
-
   return (
-    <main className="min-h-screen p-12">
-      <h1 className="text-xl font-semibold">Home</h1>
-      <div className="grid mt-6 gap-y-2">
-        <span>Name: {name}</span>
-        <span>Bio: {bio}</span>
-      </div>
+    <main className="min-h-screen grid place-items-center">
+      <h1 className="text-xl font-semibold">Hello World!</h1>
     </main>
   );
 }
