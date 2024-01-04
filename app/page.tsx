22f0f7b41edd7b430c9fdee9e53b2f261be64730
Aaryan6 <aaryanpatel683@gmail.com>
2024-01-04 12:45:44 +0530

    v0dev tut

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import { Hero } from "@/components/component/hero";
+import { LoginForm } from "@/components/component/login-form";
+
 export default function Home() {
-  return (
-    <main className="min-h-screen grid place-items-center">
-      <h1 className="text-xl font-semibold">Hello World!</h1>
-    </main>
-  );
+  return <Hero />;
 }
